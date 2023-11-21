bfd7acede2e94226be808f5f09ec0ff62c249850
ThonnyDev <97204749+ThonnyDevYT@users.noreply.github.com>
2023-11-21 12:22:32 -0500

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -51,8 +51,6 @@ class OptionsState extends MusicBeatState
 
 	function openSelectedSubstate(label:String) {
 		switch(label) {
-			case 'Note Colors':
-				openSubState(new options.NotesSubState());
 			/*case 'Controls':
 				openSubState(new options.ControlsSubState());*/
 			case 'Graphics':
